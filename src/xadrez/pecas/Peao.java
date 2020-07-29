291e29f83598329a02f933762e06b81a1478fa3d
F-SaraivaDev <saraiva@gmail.com>
2020-07-29 17:47:23 -0300

    Peão

---
+++ src/xadrez/pecas/Peao.java
@@ -0,0 +1,80 @@
+package xadrez.pecas;
+
+import jogotabuleiro.Posicao;
+import jogotabuleiro.Tabuleiro;
+import xadrez.Cor;
+import xadrez.PecaXadrez;
+
+public class Peao extends PecaXadrez {
+
+	public Peao(Tabuleiro tabuleiro, Cor cor) {
+		super(tabuleiro, cor);
+	}
+
+	@Override
+	public boolean[][] movimentoPossiveis() {
+		boolean[][] matriz = new boolean[getTabuleiro().getLinhas()][getTabuleiro()
+				.getColunas()];
+
+		Posicao p = new Posicao(0, 0);
+
+		if (getCor() == Cor.BRANCO) {
+			p.setValores(posicao.getLinha() - 1, posicao.getColuna());
+			if (getTabuleiro().posicaoExistente(p)
+					&& !getTabuleiro().haUmaPeca(p)) {
+				matriz[p.getLinha()][p.getColuna()] = true;
+			}
+			p.setValores(posicao.getLinha() - 2, posicao.getColuna());
+			Posicao p2 = new Posicao(posicao.getLinha() - 1,
+					posicao.getColuna());
+			if (getTabuleiro().posicaoExistente(p)
+					&& !getTabuleiro().haUmaPeca(p)
+					&& getTabuleiro().posicaoExistente(p2)
+					&& !getTabuleiro().haUmaPeca(p2)
+					&& getContadorMovimento() == 0) {
+				matriz[p.getLinha()][p.getColuna()] = true;
+			}
+			p.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+			if (getTabuleiro().posicaoExistente(p) && existeUmaPecaOponente(p)) {
+				matriz[p.getLinha()][p.getColuna()] = true;
+			}
+			p.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+			if (getTabuleiro().posicaoExistente(p) && existeUmaPecaOponente(p)) {
+				matriz[p.getLinha()][p.getColuna()] = true;
+			}
+
+		} else {
+			p.setValores(posicao.getLinha() + 1, posicao.getColuna());
+			if (getTabuleiro().posicaoExistente(p)
+					&& !getTabuleiro().haUmaPeca(p)) {
+				matriz[p.getLinha()][p.getColuna()] = true;
+			}
+			p.setValores(posicao.getLinha() + 2, posicao.getColuna());
+			Posicao p2 = new Posicao(posicao.getLinha() - 1,
+					posicao.getColuna());
+			if (getTabuleiro().posicaoExistente(p)
+					&& !getTabuleiro().haUmaPeca(p)
+					&& getTabuleiro().posicaoExistente(p2)
+					&& !getTabuleiro().haUmaPeca(p2)
+					&& getContadorMovimento() == 0) {
+				matriz[p.getLinha()][p.getColuna()] = true;
+			}
+			p.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+			if (getTabuleiro().posicaoExistente(p) && existeUmaPecaOponente(p)) {
+				matriz[p.getLinha()][p.getColuna()] = true;
+			}
+			p.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+			if (getTabuleiro().posicaoExistente(p) && existeUmaPecaOponente(p)) {
+				matriz[p.getLinha()][p.getColuna()] = true;
+			}
+		}
+
+		return matriz;
+	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
+
+}
